53f0f34c00ea725945060ea6086f1877aad87303
Tannaz Etminan <130409432+tannazetminan@users.noreply.github.com>
2024-03-30 01:18:53 -0700

    Adding CrossOrigin to MessageController.java

--- JobMatchingBoardBackend/src/main/java/com/group2/handyman/controller/MessageController.java
+++ JobMatchingBoardBackend/src/main/java/com/group2/handyman/controller/MessageController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
